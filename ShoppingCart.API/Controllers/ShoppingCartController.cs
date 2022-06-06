7573949ddf14fddc8f980cec7706f858688d35e6
CANER UNUTKAN <CANER.UNUTKAN@lcwaikiki.com>
2022-06-06 22:02:16 +0300

    query handler pre

---
+++ ShoppingCart.API/Controllers/ShoppingCartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/shoppingcart")]
+    public async Task<IActionResult> AddToCart([FromBody] Object request)
+    {
+        IQuerySender
+        return OK(await _qu);
+    }
+}
