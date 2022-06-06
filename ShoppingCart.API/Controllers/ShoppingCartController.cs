d2eef55b374029f351572ef514ca3145f6beb37f
CANER UNUTKAN <CANER.UNUTKAN@lcwaikiki.com>
2022-06-06 22:52:07 +0300

    controller feature

--- ShoppingCart.API/Controllers/ShoppingCartController.cs
+++ ShoppingCart.API/Controllers/ShoppingCartController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ShoppingCart.Business.Managers.ShoppingCart;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,30 @@ namespace ShoppingCart.API.Controllers
 {
     [ApiController]
     [Route("api/v1/shoppingcart")]
-    public async Task<IActionResult> AddToCart([FromBody] Object request)
+    public class ShoppingCartController : ControllerBase
     {
-        IQuerySender
-        return OK(await _qu);
+        private readonly ILogger<ShoppingCartController> _logger;
+        private readonly IShoppingCartManager _manager;
+
+        public ShoppingCartController(
+            ILogger<ShoppingCartController> logger,
+            IShoppingCartManager manager)
+        {
+            _logger = logger;
+            _manager = manager;
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> AddToCart()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = null
+            })
+            .ToArray();
+        }
     }
 }
