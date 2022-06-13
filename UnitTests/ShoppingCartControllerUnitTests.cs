46fa76f8d8d825493dc8b9c2fec304876c3ef1fb
CANER UNUTKAN <CANER.UNUTKAN@lcwaikiki.com>
2022-06-13 16:21:32 +0300

    first unit test

--- UnitTests/ShoppingCartControllerUnitTests.cs
+++ UnitTests/ShoppingCartControllerUnitTests.cs
@@ -1,7 +1,11 @@
 using Couchbase;
+using Couchbase.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using ShoppingCart.API.Controllers;
 using ShoppingCart.Business.Managers.ShoppingCart;
+using ShoppingCart.Business.Managers.ShoppingCart.Helpers;
 using ShoppingCart.Business.Models.Requests;
 using ShoppingCart.Business.Models.Responses;
 using ShoppingCart.Data.Repositories.Couchbase.SoppingCartRepository;
@@ -13,25 +17,55 @@ namespace UnitTests
 {
     public class ShoppingCartControllerUnitTests
     {
+        private readonly ILogger<ShoppingCartController> _logger;
+        private readonly IShoppingCartManager _manager;
+
+        public ShoppingCartControllerUnitTests()
+        {
+            var services = new ServiceCollection();
+
+            //add services
+            var options = new ClusterOptions
+            {
+                QueryTimeout = TimeSpan.FromSeconds(10)
+            };
+
+            services.AddCouchbase(options =>
+            {
+                options.ConnectionString = "couchbase://127.0.0.1";
+                options.Password = "password";
+                options.UserName = "Administrator";
+
+            }).AddCouchbaseBucket<IShoppingCartBucketProvider>("shoppingcart-bucket");
+
+            services.AddSingleton<IShoppingCartRepository, ShoppingCartRepository>();
+            services.AddSingleton<IShoppingCartManager, ShoppingCartManager>();
+            services.AddSingleton(typeof(ILogger<ShoppingCartController>), typeof(NullLogger<ShoppingCartController>));
+            services.AddSingleton(typeof(ILoggerFactory), typeof(NullLoggerFactory));
+            services.AddSingleton<IShoppingCartValidations, ShoppingCartValidations>();
+
+
+            //build providers
+            var serviceProvider = services.BuildServiceProvider();
+
+            //get dependincies
+            _manager = serviceProvider.GetService<IShoppingCartManager>();
+            _logger = serviceProvider.GetService<ILogger<ShoppingCartController>>();
+        }
+
         [Fact]
         public async void AddToCartUnitTest()
         {
             //arrange
-            //ILoggerFactory factory = new LoggerFactory();
-            //ILogger<ShoppingCartController> logger = new Logger<ShoppingCartController>(factory);
+            var controller = new ShoppingCartController(_logger, _manager);
 
-            //IShoppingCartRepository shoppingCartRepository = new ShoppingCartRepository(bucketProvider);
-            //IShoppingCartManager manager = new ShoppingCartManager(shoppingCartRepository);
-
-            //var controller = new ShoppingCartController(logger, manager);
-
-            ////act
-            //AddToCartRequest request = new AddToCartRequest();
-            //AddToCartResponse callResult = await controller.AddToCart(request);
+            //act
+            AddToCartRequest request = new AddToCartRequest();
+            AddToCartResponse callResult = await controller.AddToCart(request);
 
 
-            ////assert
-            //Assert.True(callResult.IsSuccess);
+            //assert
+            Assert.True(callResult.IsSuccess);
         }
     }
 }
